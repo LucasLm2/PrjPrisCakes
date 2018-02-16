e142f488ea7b284a81b3f2aa9526ba1606581166
LucasLm2 <lucaslm2>
2018-02-16 10:48:59 -0300

    Galeria pronta, efeitos refeitos

--- js/script.js
+++ js/script.js
@@ -35,33 +35,60 @@ $(document).ready(function() {
   
 });
 
-//scrollspy suavizando rolagem
-$(document).ready(function () {
-	'use strict';
-	
-	
-	
-	// Adicione scrollspy para o <body>
-	$('body').scrollspy({
-		target: ".navbar",
-		offset: 105
+//
+! function (a) {
+	"use strict";
+	a('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+		if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+			// Certifique-se de que this.hash tenha um valor antes de substituir o comportamento padrão
+			if (this.hash !== "") {
+				// Store hash
+				var e = a(this.hash);
+				// Usando o método animate () do jQuery para adicionar uma página suave
+				// O número opcional (800) especifica o número de milissegundos necessários para rolar para a área especificada
+				if ((e = e.length ? e : a("[name=" + this.hash.slice(1) + "]")).length) return a("html, body").animate({
+					scrollTop: e.offset().top - 100
+				}, 800)
+				// 800, "easeInOutExpo"), !1 deixa mais rapida a rolagem
+			}
+		}
+	}), a(".js-scroll-trigger").click(function () {
+		a(".navbar-collapse").collapse("hide")
+		// Adicione scrollspy para o <body>
+	}), a("body").scrollspy({
+		target: "#menuItens",
+		offset: 105		
 	});
-
-	// Adicione uma rolagem suave em todos os links dentro da barra de navegação
-	$("#menuItens a, .page-scroll a").on('click', function (event) {
-		// Certifique-se de que this.hash tenha um valor antes de substituir o comportamento padrão
-		if (this.hash !== "") {
-			// evitar o comportamento padrão do clique da âncora
-			event.preventDefault();
-
-			// Store hash
-			var hash = this.hash;
-
-			// Usando o método animate () do jQuery para adicionar uma página suave
-			// O número opcional (800) especifica o número de milissegundos necessários para rolar para a área especificada
-			$('html, body').animate({
-				scrollTop: ($(hash).offset().top) - 100
-			}, 800);
-		} // fim do if
-	});
-});
+	var e = function () {
+		a("#menuItens").offset().top > 100 ? a("#menuItens").addClass("navbar-shrink") : a("#menuItens").removeClass("navbar-shrink")
+	};
+		// .sr-icons animação de icones
+	e(), a(window).scroll(e), window.sr = ScrollReveal(), sr.reveal(".sr-icons", {
+		duration: 600,
+		scale: .3,
+		distance: "0px"
+		// .sr-button animação de botões
+	}, 200), sr.reveal(".sr-button", {
+		duration: 1e3,
+		delay: 200
+		// .sr-contact animação de contato
+	}), sr.reveal(".sr-contact", {
+		duration: 600,
+		scale: .3,
+		distance: "0px"
+		// .popup-gallery animação de Galeria
+	}, 300), a(".popup-gallery").magnificPopup({
+		delegate: "a",
+		type: "image",
+		tLoading: "Carregando imagem #%curr%...",
+		mainClass: "mfp-img-mobile",
+		gallery: {
+			enabled: !0,
+			navigateByImgClick: !0,
+			preload: [0, 1]
+		},
+		image: {
+			tError: '<a href="%url%">A imagem #%curr%</a> não pode ser exibida.'
+		}
+	})
+}(jQuery);
